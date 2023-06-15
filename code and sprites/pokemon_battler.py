e898dba9a5aab0434a416eeaae7bfab1f1ce508a
yobsicle <124639826+yobsicle@users.noreply.github.com>
2023-06-15 13:44:20 +1200

    no changes just testing

--- code and sprites/pokemon_battler.py
+++ code and sprites/pokemon_battler.py
@@ -1,5 +1,5 @@
 import pygame
 from pygame_functions import *
 
-pygame_functions.screenSize(600,600) 
-pygame_functions.setBackgroundColour('green')
+screenSize(600,600) 
+setBackgroundColour('green')
