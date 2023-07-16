632377349a86630a01f0b229d6bc920a43e8e823
yobsicle <toby.virtue@student.onslow.school.nz>
2023-07-16 16:37:28 +1200

    added animations for swampert and sceptile

--- pokemon_battler.py
+++ pokemon_battler.py
@@ -21,6 +21,8 @@ Enter choice here: """))
         print("Please enter a valid number from 1-3")
 background_sprite = makeSprite('sprites/battle background.png')
 blaziken_sprite = makeSprite("sprites/blaziken final full.png", 33)
+sceptile_sprite = makeSprite('sprites/sceptile final.png', 27)
+swampert_sprite = makeSprite('sprites/swampert final.png', 34)
 eevee_sprite = makeSprite("sprites/eevee.png")
 battle_menu = makeSprite("sprites/battle menu.png", 4)
 earthquake = makeLabel("EARTHQUAKE", 30, 40, 290, "black", 'Agency FB')
@@ -40,8 +42,17 @@ showSprite(eevee_sprite)
 moveSprite(eevee_sprite, 450, 100, True)
 transformSprite(eevee_sprite, 0, 0.35)
 moveSprite(blaziken_sprite, 150, 250, True)
-showSprite(blaziken_sprite)
+moveSprite(sceptile_sprite, 150, 250, True)
+moveSprite(swampert_sprite, 150, 250, True)
+if starter_choice == 1:
+    showSprite(sceptile_sprite)
+elif starter_choice == 2:
+    showSprite(blaziken_sprite)
+elif starter_choice == 3:
+    showSprite(swampert_sprite)
 transformSprite(blaziken_sprite, 0, 2.6)
+transformSprite(sceptile_sprite, 0, 2.6)
+transformSprite(swampert_sprite, 0, 2.6)
 showSprite(battle_menu)
 transformSprite(battle_menu, 0, 2.55)
 moveSprite(battle_menu, 0, 280)
@@ -52,14 +63,25 @@ attacking = False
 
 while True:
     if clock() > next_frame:
-        frame = (frame + 1)%16
-        next_frame += 40 
+        if starter_choice == 1:
+            frame = (frame + 1)%27
+            next_frame += 40 
+        elif starter_choice == 2:
+            frame = (frame + 1)%16
+            next_frame += 40 
+        elif starter_choice == 3:
+            frame = (frame + 1)%34
+            next_frame += 40 
 
     tick(12)
     if keyPressed("y"):
         attacking = True
-    else:
-        changeSpriteImage(blaziken_sprite,0 *16 + frame)
+    elif starter_choice == 1:
+        changeSpriteImage(sceptile_sprite,0 * 13 + frame)
+    elif starter_choice == 2:
+        changeSpriteImage(blaziken_sprite,0 * 16 + frame)
+    elif starter_choice == 3:
+        changeSpriteImage(swampert_sprite,0 * 34 + frame)
 
     if keyPressed("right"):
         current_selection = (current_selection + 1)%4
@@ -75,8 +97,19 @@ while True:
         changeSpriteImage(battle_menu, current_selection)
 
     if attacking == True:
-        for i in range(16):
-            changeSpriteImage(blaziken_sprite, 16 + i)
-            pause(20, True)
+        if starter_choice == 1:
+            for i in range(10):
+                changeSpriteImage(sceptile_sprite, 10 + i)
+                pause(40, True)
+            attacking = False
+        elif starter_choice == 2:
+            for i in range(16):
+                changeSpriteImage(blaziken_sprite, 16 + i)
+                pause(20, True)
+            attacking = False
+        elif starter_choice == 3:
+            for i in range(10):
+                changeSpriteImage(swampert_sprite, i)
+                pause(40, True)
             attacking = False
 endWait()
